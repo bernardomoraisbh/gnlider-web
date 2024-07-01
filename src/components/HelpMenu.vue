58fa6eaf88e83dd42e12c043b30b2946b91991c4
Bernardo Morais <bernardomalfredo@hotmail.com>
2024-07-01 03:44:35 -0300

    Otimizacoes no contato pelo help menu

--- src/components/HelpMenu.vue
+++ src/components/HelpMenu.vue
@@ -3,13 +3,24 @@
     <q-btn flat label="Ajuda" icon="help">
       <q-menu>
         <q-list style="min-width: 100px">
-          <q-item to="/contato" clickable v-close-popup>
+          <q-item tag="a" href="https://wa.me/5531994760839" target="_blank" clickable v-close-popup>
+            <q-item-section avatar>
+              <q-icon name="fab fa-whatsapp" />
+            </q-item-section>
+
+            <q-item-section>
+              Contato
+            </q-item-section>
+          </q-item>
+          <q-separator />
+          <q-item tag="a" href="mailto:naoresponda@gnlider.com.br"
+                  target="_blank" clickable v-close-popup>
             <q-item-section avatar>
               <q-icon name="mail" />
             </q-item-section>
 
             <q-item-section>
-              Contato
+              Enviar Email
             </q-item-section>
           </q-item>
           <q-separator />
